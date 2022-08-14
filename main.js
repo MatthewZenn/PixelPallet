110ef5c1a55cdd5fca879e5fc13d3d4a68fcdd03
Matthew Zenn <spyguy57@gmail.com>
2022-08-14 18:46:55 -0400

    Hue input not working

--- main.js
+++ main.js
@@ -2,15 +2,32 @@ var color1 = document.getElementById("col1");
 var color2 = document.getElementById("col2");
 var color3 = document.getElementById("col3");
 var color4 = document.getElementById("col4");
+var i = 0;
+var huel = 0;
+
+document.getElementById("hue-shift").addEventListener("input", function(){
+   i = document.getElementById("hue-shift").value;
+});
+
+document.getElementById("hue").addEventListener("input", function() {
+    huel = document.getElementById("hue").value;
+ });
 
 document.getElementById("gen").onclick = function() {generate()};
 
 function generate() {
-    var value = "0"
-    var value = Math.floor(Math.random() * (255 - 0) + 0);
-    
-    color1.style.backgroundColor = `hsl(${value}, 100%, 20%)`;
-    color2.style.backgroundColor = `hsl(${value}, 100%, 40%)`;
-    color3.style.backgroundColor = `hsl(${value}, 100%, 60%)`;
-    color4.style.backgroundColor = `hsl(${value}, 100%, 80%)`;
+    if (huel = 0) {
+        huel = Math.floor(Math.random() * (360));
+
+        color1.style.backgroundColor = `hsl(${huel+(i*3)}, 100%, 20%)`;
+        color2.style.backgroundColor = `hsl(${huel+(i*2)}, 100%, 40%)`;
+        color3.style.backgroundColor = `hsl(${huel+i}, 100%, 60%)`;
+        color4.style.backgroundColor = `hsl(${huel}, 100%, 80%)`; 
+    }
+    else {
+        color1.style.backgroundColor = `hsl(${huel+(i*3)}, 100%, 20%)`;
+        color2.style.backgroundColor = `hsl(${huel+(i*2)}, 100%, 40%)`;
+        color3.style.backgroundColor = `hsl(${huel+i}, 100%, 60%)`;
+        color4.style.backgroundColor = `hsl(${huel}, 100%, 80%)`;
+    }
 }
